8ce0c2b1dd863c72378ac175d45bddcfb0d90961
ANNA MARCINIAK - WYKA <a.marciniak-wyka@cyfrowy.org>
2023-01-15 22:18:08 +0100

    #35 Migracja

---
+++ database/migrations/2023_01_15_203935_add_phone_number_and_surname_columns_to_users_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string(column: 'surname')->after(column: 'name')->nullable();
+            $table->string(column: 'phone_number', length: 20)->after(column: 'surname')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn(['surname', 'phone_number']);
+        });
+    }
+};
